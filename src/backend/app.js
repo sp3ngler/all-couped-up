111e42276cb7f980a89e772e03ab18f589bdeb65
Adam Feng <adam.feng@stonybrook.edu>
2024-02-03 18:03:24 -0500

    First Commit

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,29 +1,37 @@
-/*
-THE ENTRY POINT or main application file for a Node.js project.
-
-In a Node.js application, app.js typically includes the following responsibilities:
-
-1.Server Setup: Creating an HTTP server using the built-in http or express module to handle incoming requests and send responses.
-2.Routing: Defining routes to handle different HTTP methods (GET, POST, etc.) and specifying the corresponding logic to execute when those routes are accessed.
-3.Middleware Configuration: Applying middleware functions to handle tasks such as request parsing, authentication, logging, etc. Middleware functions are executed before the final request handler.
-4.Database Connections: Establishing connections to databases or other external services that the application interacts with.
-5.Configuration: Setting up and configuring the application, including environment variables, constants, and other settings.
-*/
 
 const express = require('express');
 const app = express();
+
+//socket.io setup
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
 const port = 3000;
 
-// Middleware to parse incoming JSON requests
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static('src'));
 
-// Route definition
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.sendFile(__dirname + '/src/frontend/tmp.html');
+  });
+
+const players = {}
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    players[socket.id] = {
+        x: 100,
+        y: 100
+    }
+
+    console.log(players)
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+server.listen(port, () => {
+    console.log(`Coup listening on port ${port}`);
 });
 
+console.log('server did not load')
---
+++ src/frontend/js/tmp.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+const socket = io()
+
+const scoreEl = document.querySelector('#scoreEl')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 10, 'white')
+
+let animationId
+function animate() {
+  animationId = requestAnimationFrame(animate)
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+
+  player.draw()
+}
+
+animate()
