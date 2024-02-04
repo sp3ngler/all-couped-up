86c095f6e68761bdc450629f5a9f5a89d507a1ab
Adam Feng <adam.feng@stonybrook.edu>
2024-02-03 19:34:45 -0500

    Added connect and disconnect functions

--- src/frontend/js/tmp.js
+++ src/frontend/js/tmp.js
@@ -12,6 +12,25 @@ const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 10, 'white')
+const players = {}
+
+socket.on('updatePlayers', (backendPlayers) =>{
+  for (const id in backendPlayers){
+    const backendPlayer = backendPlayers[id]
+
+    if (!players[id]) {
+      players[id] = new Player(backendPlayer.x, backendPlayer.y, 10, 'white')
+    }
+  }
+  
+  for(const id in players) {
+    if (!backendPlayers[id]){
+      delete players[id]
+    }
+  }
+
+  console.log(players)
+})
 
 let animationId
 function animate() {
@@ -19,7 +38,10 @@ function animate() {
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
 
-  player.draw()
+  for(const id in players){
+    const player = players[id]
+    player.draw()
+  }
 }
 
 animate()
